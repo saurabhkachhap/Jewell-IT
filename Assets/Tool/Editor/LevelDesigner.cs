8f5bfa2021b2d7243f8ce992926f68b039e04aa9
saurabhkachhap <saurabhkachhap122@gmail.com>
2020-07-23 11:00:42 +0530

    Designs can now be Saved

--- Assets/Tool/Editor/LevelDesigner.cs
+++ Assets/Tool/Editor/LevelDesigner.cs
@@ -18,16 +18,17 @@ public class LevelDesignerTool : EditorWindow
     SerializedProperty propTheme;
     GameObject[] tempelates;
 
-    GameObject selectedTempelateGameObject;
-    //private int[] tempelateData;
     int selectedTempelateIndex = -1;
     //int last = 0;
     GameObject newGem;
     int sectionCount;
     GameObject[] section;
     GameObject newGameObject;
-
-
+    public string savePath = "Assets/Jewellery/Prefabs/";
+    SerializedProperty propSavePath;
+    public string fileName;
+    SerializedProperty propFileName;
+    SerializedObject thisObj;
 
     private void OnEnable()
     {
@@ -41,6 +42,10 @@ public class LevelDesignerTool : EditorWindow
         string[] guids = AssetDatabase.FindAssets("t:prefab", new string[] { "Assets/Tool/Design Tempelate"});
         IEnumerable<string> paths = guids.Select(AssetDatabase.GUIDToAssetPath);
         tempelates = paths.Select(AssetDatabase.LoadAssetAtPath<GameObject>).ToArray();
+
+        thisObj = new SerializedObject(this);
+        propSavePath = thisObj.FindProperty("savePath");
+        propFileName = thisObj.FindProperty("fileName");
     }
 
     private void OnGUI()
@@ -59,6 +64,7 @@ public class LevelDesignerTool : EditorWindow
                 propSize = so.FindProperty("size");
                 propTheme = so.FindProperty("theme");
                 //gameObject = design.jewellerySize[(int)design.size];
+                fileName = "Design_" + design.theme.name + "_L00";
                 EditorUtility.SetDirty(design);
             }
         }
@@ -84,7 +90,7 @@ public class LevelDesignerTool : EditorWindow
                     {
                         if (EditorGUI.EndChangeCheck())
                         {
-                            selectedTempelateGameObject = tempelates[i];
+                            //selectedTempelateGameObject = tempelates[i];
                             selectedTempelateIndex = i;
                             sectionCount = design.tempelateDesignData[i].uniqueSection;
                             section = new GameObject[sectionCount];
@@ -94,51 +100,26 @@ public class LevelDesignerTool : EditorWindow
                 }
             }
         }
-            
-        
-        if(GUILayout.Button("Generate Design"))
+        so.ApplyModifiedProperties();
+
+        if (GUILayout.Button("Generate Design"))
         {
-            if(newGameObject != null)
-            {
-                DestroyImmediate(newGameObject);
-            }
-            newGameObject = new GameObject();
-            var selectedSize = design.jewellerySize[(int)design.size];
-            var selectedTempelateDesignData = design.tempelateDesignData[selectedTempelateIndex]
-                                                .tempelateSizeVarient[(int)design.size].designData;
-
-            var themeSlot = design.theme;
-            for (int j = 0; j < section.Length; j++)
-            {
-                section[j] = themeSlot.gem[Random.Range(0, themeSlot.gem.Length)];
-            }
-
-            for (int i = 0; i < selectedTempelateDesignData.Length; i++)
-            {
-                var current = selectedTempelateDesignData[i];
-               
-                switch (current)
-                {
-                    case 1:
-                        newGem = section[0];
-                        break;
-                    case 2:
-                        newGem = section[1];
-                        break;
-                    case 3:
-                        newGem = section[2];
-                        break;
-                }
-                               
-                var newSpawnedGem = (GameObject)PrefabUtility.InstantiatePrefab(newGem,newGameObject.transform);             
-                var childTransform = selectedSize.transform.GetChild(i);
-               
-                newSpawnedGem.transform.position = childTransform.position;
-                newSpawnedGem.transform.rotation = childTransform.rotation;
-                        }
-            gameObject = newGameObject;
+            GenerateDesign();
         }
-        GUILayout.Button("Save");
+
+        thisObj.Update();
+        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+        {
+            EditorGUILayout.PropertyField(propSavePath);
+            EditorGUILayout.PropertyField(propFileName);
+
+            if (GUILayout.Button("Save"))
+            {
+                var newPath = savePath + fileName +".prefab";
+                PrefabUtility.SaveAsPrefabAssetAndConnect(newGameObject, newPath, InteractionMode.UserAction);
+            }
+        }
+        thisObj.ApplyModifiedProperties();
 
         if (gameObject != null )
         {
@@ -153,8 +134,49 @@ public class LevelDesignerTool : EditorWindow
                 gameObjectEditor.OnPreviewGUI(GUILayoutUtility.GetRect(500, 500, EditorStyles.foldout), EditorStyles.whiteLabel);
             }
         } 
-        so.ApplyModifiedProperties();
-              
+    }
+
+    private void GenerateDesign()
+    {
+        if (newGameObject != null)
+        {
+            DestroyImmediate(newGameObject);
+        }
+        newGameObject = new GameObject();
+        var selectedSize = design.jewellerySize[(int)design.size];
+        var selectedTempelateDesignData = design.tempelateDesignData[selectedTempelateIndex]
+                                            .tempelateSizeVarient[(int)design.size].designData;
+
+        var themeSlot = design.theme;
+        for (int j = 0; j < section.Length; j++)
+        {
+            section[j] = themeSlot.gem[Random.Range(0, themeSlot.gem.Length)];
+        }
+
+        for (int i = 0; i < selectedTempelateDesignData.Length; i++)
+        {
+            var current = selectedTempelateDesignData[i];
+
+            switch (current)
+            {
+                case 1:
+                    newGem = section[0];
+                    break;
+                case 2:
+                    newGem = section[1];
+                    break;
+                case 3:
+                    newGem = section[2];
+                    break;
+            }
+
+            var newSpawnedGem = (GameObject)PrefabUtility.InstantiatePrefab(newGem, newGameObject.transform);
+            var childTransform = selectedSize.transform.GetChild(i);
+
+            newSpawnedGem.transform.position = childTransform.position;
+            newSpawnedGem.transform.rotation = childTransform.rotation;
+        }
+        gameObject = newGameObject;
     }
 
     private void OnDestroy()
